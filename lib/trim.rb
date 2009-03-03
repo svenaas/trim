c85822d60cfff88c203e2ece43ab345890722093
Sven Aas <saas@mtholyoke.edu>
2009-03-03 17:21:52 -0500

    Implemented username/password authentication on URL shortening methods. Implemented account verification. Updated docs. Bumped version to 0.2.0.

--- lib/trim.rb
+++ lib/trim.rb
@@ -7,12 +7,10 @@ require 'libxml'
 module Trim
 
 	# 
-	# Alias for Trim::trim_simple
+	# Alias for #Trim::trim_simple
 	#
-	def Trim::trim(url)
-		response = nil
-	  open("http://api.tr.im/api/trim_simple?url=#{url}") {|http| response = http.read }
-	  response.strip!.size > 0 ? response : nil
+	def Trim::trim(url, username = nil, password = nil)		
+	  trim_simple(url, username, password)	  
   end
 
   #  
@@ -20,11 +18,22 @@ module Trim
 	#
 	#   trimmed_url = Trim.trim_simple('http://sample.com/some/long/url')
 	#
-	# Utilizes the tr.im API method trim_simple
+	# If a +username+ and +password+ are provided they will be passed to tr.im
+	# for authentication. NOTE: as of this writing (3 Mar 2009) tr.im does 
+	# not support SSL so this username and password will be sent over
+	# unencrypted HTTP.
+	# 
+	# If the username and password provided are not valid, tr.tim will still
+	# generate a shortened URL, but will do so as if no username and password
+	# were provided. The account credentials can be verified with #Trim::valid_account?
+	#
+	# This method utilizes the tr.im API method trim_simple
 	#	
-	def Trim::trim_simple(url)
+	def Trim::trim_simple(url, username = nil, password = nil)
 		response = nil
-	  open("http://api.tr.im/api/trim_simple?url=#{url}") {|http| response = http.read }
+		request_url = "http://api.tr.im/api/trim_simple?url=#{url}"
+		request_url += "&username=#{username}&password=#{password}" if (username and password)
+	  open(request_url) {|http| response = http.read }
 	  response.strip!.size > 0 ? response : nil
   end
 
@@ -32,13 +41,43 @@ module Trim
 	# Returns a trimmed URL based on +url+, or nil if any error occurs.
 	#
 	#   trimmed_url = Trim.trim_url('http://sample.com/some/long/url')
+	#
+	# If a +username+ and +password+ are provided they will be passed to tr.im
+	# for authentication. NOTE: as of this writing (3 Mar 2009) tr.im does 
+	# not support SSL so this username and password will be sent over
+	# unencrypted HTTP.
+	# 
+	# If the username and password provided are not valid, tr.tim will still
+	# generate a shortened URL, but will do so as if no username and password
+	# were provided. The account credentials can be verified with #Trim::valid_account?
 	#	
 	# Utilizes the tr.im API method trim_url.xml
 	#
-	def Trim::trim_url(url)
+	def Trim::trim_url(url, username = nil, password = nil)
 		xml_response = nil
-	  open("http://api.tr.im/api/trim_url.xml?url=#{url}") {|http| xml_response = http.read }
+		request_url = "http://api.tr.im/api/trim_url.xml?url=#{url}"
+		request_url += "&username=#{username}&password=#{password}" if (username and password)
+	  open(request_url) {|http| xml_response = http.read }
 		get_url_from_xml_response(xml_response)
+  end
+  
+  #  
+	# Verifies that the provided +username+ and +password+ successfully 
+	# validate as an account known to tr.im
+	#
+	#   is_valid_account = Trim.valid_account?('good_username', 'good_password')
+	#
+	# NOTE: as of this writing (3 Mar 2009) tr.im does not support SSL so this 
+	# username and password will be sent over unencrypted HTTP.
+	# 
+	# Utilizes the tr.im API method verify.xml
+	#  
+  def Trim::valid_account?(username, password)
+		xml_response = nil
+		request_url = "http://api.tr.im/api/verify.xml?username=#{username}&password=#{password}"
+	  open(request_url) {|http| xml_response = http.read }
+    status_code = get_status_code_from_xml_response(xml_response)
+    "200" == status_code
   end
 
 private  
@@ -47,4 +86,9 @@ private
     urls = parser.parse.find('url')
     urls.size > 0 ? urls.first.content : nil
 	end  
+	
+	def Trim::get_status_code_from_xml_response(xml_response)	
+    parser = LibXML::XML::Parser.string xml_response
+    status = parser.parse.root.attributes['code']      
+	end 
 end
--- test/unit/trim_test.rb
+++ test/unit/trim_test.rb
@@ -4,99 +4,142 @@ require 'mocha'
 class TrimTest < Test::Unit::TestCase
 
 	VALID_SHORTENED_URL = "http://tr.im/OK"
-	
-	context "A valid URL" do
-		
-		setup do
-			@long_url = 'http://www.google.com/'
-		end
-		
-		context "A trim_simple request" do  	
+
+  context "With a valid account" do    
+    setup do
+      @username = 'user'
+      @password = 'pass'
+      @long_url = 'http://www.google.com/'
+    end
+
+    context "account verification" do
+      should "succeed" do 
+        Trim.expects(:valid_account?).with(@username, @password).returns(true)
+        assert Trim.valid_account?(@username, @password)
+      end
+    end        
+    
+    context "a trim_simple request" do 
 			should "provide a working shortened URL" do 
-			  Trim.expects(:trim_simple).with(@long_url).returns(VALID_SHORTENED_URL)
-				trimmed_url = Trim.trim_simple @long_url
+			  Trim.expects(:trim_simple).with(@long_url, @username, @password).returns(VALID_SHORTENED_URL)
+				trimmed_url = Trim.trim_simple @long_url, @username, @password
 				assert_equal VALID_SHORTENED_URL, trimmed_url 
 			end
 		end
 		
-    context "A trim_url request" do    
-      should "provide a working shortened URL" do 
-			  Trim.expects(:trim_url).with(@long_url).returns(VALID_SHORTENED_URL)
-        trimmed_url = Trim.trim_url @long_url
-        assert_equal VALID_SHORTENED_URL, trimmed_url 
-      end
+    context "a trim_url request" do 
+			should "provide a working shortened URL" do 
+			  Trim.expects(:trim_url).with(@long_url, @username, @password).returns(VALID_SHORTENED_URL)
+				trimmed_url = Trim.trim_url @long_url, @username, @password
+				assert_equal VALID_SHORTENED_URL, trimmed_url 
+			end
+		end		
+  end      
+
+  context "With an invalid account" do    
+    setup do
+      @username = 'baduser'
+      @password = 'badpass'
     end
-    
+
+    context "account verification" do
+      should "fail" do 
+        Trim.expects(:valid_account?).with(@username, @password).returns(false)
+        assert ! Trim.valid_account?(@username, @password)
+      end
+    end        
   end
-   
-  context "An invalid URL" do
+	
+	context "Anonymously" do	  	  
+  	context "using a valid URL" do  		
+  		setup do
+  			@long_url = 'http://www.google.com/'
+  		end
+  		
+  		context "a trim_simple request" do  	
+  			should "provide a working shortened URL" do 
+  			  Trim.expects(:trim_simple).with(@long_url).returns(VALID_SHORTENED_URL)
+  				trimmed_url = Trim.trim_simple @long_url
+  				assert_equal VALID_SHORTENED_URL, trimmed_url 
+  			end
+  		end
+  		
+      context "a trim_url request" do    
+        should "provide a working shortened URL" do 
+  			  Trim.expects(:trim_url).with(@long_url).returns(VALID_SHORTENED_URL)
+          trimmed_url = Trim.trim_url @long_url
+          assert_equal VALID_SHORTENED_URL, trimmed_url 
+        end
+      end
+      
+    end
      
-    setup do
-      @long_url = 'www'
-    end
-  
-    context "A trim_simple request" do      
-      should "return nil" do 
-        Trim.expects(:trim_simple).with(@long_url).returns(nil)
-        trimmed_url = Trim.trim_simple @long_url
-        assert_nil trimmed_url
+    context "using an invalid URL" do       
+      setup do
+        @long_url = 'www'
       end
-    end
-  
-    context "A trim_url request" do      
-      should "return nil" do 
-        Trim.expects(:trim_url).with(@long_url).returns(nil)
-        trimmed_url = Trim.trim_url @long_url
-        assert_nil trimmed_url
-      end
-    end
-  
-    end
-  
-  context "An empty URL" do
     
-    setup do
+      context "a trim_simple request" do      
+        should "return nil" do 
+          Trim.expects(:trim_simple).with(@long_url).returns(nil)
+          trimmed_url = Trim.trim_simple @long_url
+          assert_nil trimmed_url
+        end
+      end
+    
+      context "a trim_url request" do      
+        should "return nil" do 
+          Trim.expects(:trim_url).with(@long_url).returns(nil)
+          trimmed_url = Trim.trim_url @long_url
+          assert_nil trimmed_url
+        end
+      end
+    
+    end
+    
+    context "using an empty URL" do      
+      setup do
         @long_url = ''
       end
-  
-    context "A trim_simple request" do      
-      should "return nil" do 
-        Trim.expects(:trim_simple).with('').returns(nil)
-        trimmed_url = Trim.trim_simple @long_url
-        assert_nil trimmed_url
-      end
-    end
-  
-    context "A trim_url request" do      
-      should "return nil" do 
-        Trim.expects(:trim_url).with('').returns(nil)
-        trimmed_url = Trim.trim_url @long_url
-        assert_nil trimmed_url
-      end
-    end
-  
-    end
-  
-  context "A nil URL" do
     
-    setup do
+      context "a trim_simple request" do      
+        should "return nil" do 
+          Trim.expects(:trim_simple).with('').returns(nil)
+          trimmed_url = Trim.trim_simple @long_url
+          assert_nil trimmed_url
+        end
+      end
+    
+      context "a trim_url request" do      
+        should "return nil" do 
+          Trim.expects(:trim_url).with('').returns(nil)
+          trimmed_url = Trim.trim_url @long_url
+          assert_nil trimmed_url
+        end
+      end
+    
+    end
+    
+    context "using a nil URL" do      
+      setup do
         @long_url = nil
       end
-  
-    context "A trim_simple request" do      
-      should "return nil" do 
-        trimmed_url = Trim.trim @long_url
-        assert_nil trimmed_url
+    
+      context "a trim_simple request" do      
+        should "return nil" do 
+          Trim.expects(:trim_simple).with(nil).returns(nil)
+          trimmed_url = Trim.trim_simple @long_url
+          assert_nil trimmed_url
+        end
       end
+    
+      context "a trim_url request" do      
+        should "return nil" do 
+          Trim.expects(:trim_url).with(nil).returns(nil)
+          trimmed_url = Trim.trim_url @long_url
+          assert_nil trimmed_url
+        end
+      end    
     end
-  
-    context "A trim_url request" do      
-      should "return nil" do 
-        trimmed_url = Trim.trim @long_url
-        assert_nil trimmed_url
-      end
-    end
-  
   end
-
 end
